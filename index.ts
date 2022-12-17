b5ca2ebec74c6cc8cd6ff4627b55e82bc27aeb76
Julian George <juliancgeorge@gmail.com>
2022-12-16 21:24:58 -0500

    cached bug test

--- index.ts
+++ index.ts
@@ -22,6 +22,7 @@ mongoose
   });
 
 if (process.env.CACHED) {
+  console.log("cached env true");
   parseResults(load(fs.readFileSync("cached/results-browser.html")));
 } else {
   scrapeClient(`/results?offset=${RESULT_OFFSET}`).then((resultsPage) => {
